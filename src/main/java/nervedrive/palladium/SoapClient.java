143d41699d5ef6e57d683410ea97e1b470dbc3fc
Jibin Joy <jibin.joy@thoughtworks.com>
2012-12-02 23:56:37 +0530

    Initial Commit

---
+++ src/main/java/nervedrive/palladium/SoapClient.java
@@ -0,0 +1,11 @@
+package nervedrive.palladium;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jibinj
+ * Date: 02/12/12
+ * Time: 11:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SoapClient {
+}
